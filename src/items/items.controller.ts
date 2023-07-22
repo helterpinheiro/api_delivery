43240175db396439c7db06214beaae9d3efbb72e
Helter <helterpinheiro@gmail.com>
2023-07-22 11:48:14 -0300

    delete e update items funcionais

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -4,10 +4,17 @@ import {
   Post,
   UsePipes,
   ValidationPipe,
+  Get,
+  UseGuards,
+  Param,
+  Put,
+  Delete,
 } from '@nestjs/common';
 import { CriarItemDTO } from './dtos/criar-item.dto';
 import { ItemsService } from './items.service';
 import { Item } from './interfaces/item.interface';
+import { JwtAuthGuard } from 'src/auth/jwt.auth-guard';
+import { ValidacaoParametroPipe } from 'src/common/pipes/validacao-parametros.pipe';
 
 @Controller('api/v1/items')
 export class ItemsController {
@@ -17,5 +24,36 @@ export class ItemsController {
   @UsePipes(ValidationPipe)
   async createItem(@Body() criarItemDTO: CriarItemDTO): Promise<Item> {
     return await this.itemsService.createItem(criarItemDTO);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  async listAllItems(): Promise<Array<Item>> {
+    return this.itemsService.listAllItems();
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  async listItemForId(
+    @Param('_id', ValidacaoParametroPipe) _id: string,
+  ): Promise<Item> {
+    return this.itemsService.listItemForId(_id);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Put('/:_id')
+  async updateItem(
+    @Body() criarItemDTO: CriarItemDTO,
+    @Param('_id', ValidacaoParametroPipe) _id: string,
+  ): Promise<Item> {
+    return this.itemsService.updateItem(_id, criarItemDTO);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete('/:_id')
+  async deleteItem(
+    @Param('_id', ValidacaoParametroPipe) _id: string,
+  ): Promise<void> {
+    this.itemsService.deleteItem(_id);
   }
 }
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { CriarItemDTO } from './dtos/criar-item.dto';
 import { Item } from './interfaces/item.interface';
 import { v4 as uuidv4 } from 'uuid';
@@ -15,5 +15,38 @@ export class ItemsService {
   async createItem(criarItemDTO: CriarItemDTO): Promise<Item> {
     const createdItem = new this.itemModel(criarItemDTO);
     return await createdItem.save();
+  }
+
+  async listAllItems(): Promise<Array<Item>> {
+    return await this.itemModel.find().exec();
+  }
+
+  async listItemForId(_id: string): Promise<Item> {
+    const item = await this.itemModel.findOne({ _id }).exec();
+    if (!item) {
+      throw new NotFoundException(`item with id ${_id} not found!`);
+    }
+
+    return item;
+  }
+
+  async updateItem(_id: any, criarItemDTO: CriarItemDTO): Promise<Item> {
+    const item = await this.itemModel.findOne({ _id }).exec();
+    if (!item) {
+      throw new NotFoundException(`item with id ${_id} not found!`);
+    }
+
+    return await this.itemModel
+      .findOneAndUpdate({ _id }, { $set: criarItemDTO })
+      .exec();
+  }
+
+  async deleteItem(_id: any): Promise<void> {
+    const item = await this.itemModel.findOne({ _id }).exec();
+    if (!item) {
+      throw new NotFoundException(`item with id ${_id} not found!`);
+    }
+
+    await this.itemModel.deleteOne({ _id }).exec();
   }
 }
